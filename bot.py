71f1fda65fc875cc700fd063dc44d1d955011364
SUDIPBAUR <sudipbaur2@gmail.com>
2025-04-20 11:36:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ import os
 
 # Binance setup with error handling
 def setup_exchange():
-    api_key = os.getenv("zSytUpX7UqgSZH2Ig9DrTpEQTR77wBqTwldiLIrwoJWpXUU1ksUAywdxUWVMtCsm")
-    api_secret = os.getenv("orEZ15ayLiAfBjwYuEDLlTsNwrp4hIec8GSyWvOqmEbImpzsjJU9PmKbpxRqijzu")
+    api_key = "zSytUpX7UqgSZH2Ig9DrTpEQTR77wBqTwldiLIrwoJWpXUU1ksUAywdxUWVMtCsm"
+    api_secret = "orEZ15ayLiAfBjwYuEDLlTsNwrp4hIec8GSyWvOqmEbImpzsjJU9PmKbpxRqijzu"
     
     if not api_key or not api_secret:
         st.error("Binance API keys not found in environment variables!")
